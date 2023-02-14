f1534eb0531ac13e47a65f64f3de9e1d810dcb08
ValkoHappy <11081108cat@gmail.com>
2023-02-14 18:40:17 +0300

    +

---
+++ Assets/Package/RTS_Camera/Demo/TargetSelector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using RTS_Cam;
+
+[RequireComponent(typeof(RTS_Camera))]
+public class TargetSelector : MonoBehaviour
+{
+    private RTS_Camera _cam;
+    private Camera _camera;
+    public string TargetTag;
+
+    private void Start()
+    {
+        _cam = GetComponent<RTS_Camera>();
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag(TargetTag))
+                    _cam.SetTarget(hit.transform);
+                else
+                    _cam.ResetTarget();
+            }
+        }
+    }
+}
