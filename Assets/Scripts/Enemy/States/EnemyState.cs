d2721362bdb2832b18e424651a1022f9feda2d2d
ValkoHappy <11081108cat@gmail.com>
2023-04-21 21:11:30 +0300

    fixed bugs in the EnemyState class

--- Assets/Scripts/Enemy/States/EnemyState.cs
+++ Assets/Scripts/Enemy/States/EnemyState.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class EnemyState : MonoBehaviour
 {
     [SerializeField] private EnemyTransition[] _transitions;
-    public EnemyTransition[] Transitions => _transitions;
+
     public PeacefulConstruction PeacefulConstruction { get; private set; }
     public Animator Animator { get; private set; }
     public Rigidbody Rigidbody { get; private set; }
+    public EnemyTransition[] Transitions => _transitions;
 
     public void Enter(PeacefulConstruction peacefulConstruction, Animator animator, Rigidbody rigidbody)
     {
