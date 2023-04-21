63261ef9b79d2e7bfa072b6958171340113c846f
ValkoHappy <11081108cat@gmail.com>
2023-04-21 18:21:37 +0300

    fixed bugs in the StartCameraAnimation class

--- Assets/Scripts/Camera/StartCameraAnimation.cs
+++ Assets/Scripts/Camera/StartCameraAnimation.cs
@@ -5,6 +5,9 @@ public class StartCameraAnimation : MonoBehaviour
 {
     [SerializeField] private float _duration;
 
+    private int rotationAngle = 360;
+    private int numberOfRepetitions = -1;
+
     private void Start()
     {
         RotationCamera();
@@ -12,6 +15,6 @@ public class StartCameraAnimation : MonoBehaviour
 
     public void RotationCamera()
     {
-        transform.DOLocalRotate(new Vector3(0, 360, 0), _duration, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
+        transform.DOLocalRotate(new Vector3(0, rotationAngle, 0), _duration, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(numberOfRepetitions);
     }
 }
