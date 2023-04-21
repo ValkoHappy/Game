d001d501138c7ffcb275bd077126cd8fe25cacdd
ValkoHappy <11081108cat@gmail.com>
2023-04-21 20:50:24 +0300

    fixed bugs in the HealthContainer class

--- Assets/Scripts/HealthContainer.cs
+++ Assets/Scripts/HealthContainer.cs
@@ -6,11 +6,12 @@ public class HealthContainer : MonoBehaviour
     [SerializeField] private int _health;
 
     private int _maxHealth;
-    public int Health => _health;
+
     public event UnityAction<int> HealthChanged;
     public event UnityAction MaxHealthChanged;
     public event UnityAction Died;
 
+    public int Health => _health;
     public int MaxHealth => _maxHealth;
 
     private void Awake()
@@ -27,7 +28,6 @@ public class HealthContainer : MonoBehaviour
             _health = 0;
             Died?.Invoke();
         }
-
         HealthChanged?.Invoke(_health);
     }
 
