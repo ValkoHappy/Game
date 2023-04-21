bc8176cd2a549c5fdf05c54f7be172fcafafe466
ValkoHappy <11081108cat@gmail.com>
2023-04-21 20:56:43 +0300

    fixed bugs in the RecoilAnimation class

--- Assets/Scripts/Weapons/Turret/RecoilAnimation.cs
+++ Assets/Scripts/Weapons/Turret/RecoilAnimation.cs
@@ -5,9 +5,10 @@ public class RecoilAnimation : MonoBehaviour
 {
     [SerializeField] private float _recoilDistance;
     [SerializeField] private float _recoilSpeed;
+    [SerializeField] private int _numberOfRepetitions = 2;
 
     public void StartRecoil(float delayBetween)
     {
-        transform.DOLocalMoveZ(transform.localPosition.z - _recoilDistance, delayBetween / _recoilSpeed).SetLoops(2, LoopType.Yoyo);
+        transform.DOLocalMoveZ(transform.localPosition.z - _recoilDistance, delayBetween / _recoilSpeed).SetLoops(_numberOfRepetitions, LoopType.Yoyo);
     }
 }
