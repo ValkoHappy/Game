fcad26d9c92a414009a6fa2dadc8ef6c910fdbca
ValkoHappy <11081108cat@gmail.com>
2023-04-22 15:51:13 +0300

    correction in the VictoryScreen class

--- Assets/Scripts/UI/Screen/VictoryScreen.cs
+++ Assets/Scripts/UI/Screen/VictoryScreen.cs
@@ -7,17 +7,29 @@ public class VictoryScreen : UIScreenAnimator
     [SerializeField] private Button _resumeButton;
     [SerializeField] private Button _bonusButton;
 
+    [SerializeField] private Spawner _spawner;
+    [SerializeField] private EnemyHandler _enemyHandler;
+    [SerializeField] private BuildingsHandler _buildingsHandler;
+    [SerializeField] private BuildingsGrid _buildingsGrid;
+    [SerializeField] private SaveSystem _saveSystem;
+    [SerializeField] private MovingCameraSpawnEnemies _movingCameraSpawnEnemies;
+    [SerializeField] private LevelReward _levelReward;
+
     public event UnityAction ResumeButtonClick;
     public event UnityAction BonusButtonClick;
 
     private void OnEnable()
     {
+        _enemyHandler.AllEnemiesKilled += OpenScreen;
+
         _resumeButton.onClick.AddListener(OnResumeButton);
         _bonusButton.onClick.AddListener(OnBonusButton);
     }
 
     private void OnDisable()
     {
+        _enemyHandler.AllEnemiesKilled -= OpenScreen;
+
         _resumeButton.onClick.RemoveListener(OnResumeButton);
         _bonusButton.onClick.RemoveListener(OnBonusButton);
     }
@@ -25,10 +37,28 @@ public class VictoryScreen : UIScreenAnimator
     public void OnResumeButton()
     {
         ResumeButtonClick?.Invoke();
+        OnMenuAfterFightScreen();
+        _levelReward.ClaimReward();
     }
 
     public void OnBonusButton()
     {
         BonusButtonClick?.Invoke();
+        OnMenuAfterFightScreen();
+        _levelReward.ClaimDoubleReward();
+    }
+
+    private void OnMenuAfterFightScreen()
+    {
+        _spawner.ShowLevel();
+        _enemyHandler.OnDestroyEnemies();
+        _buildingsHandler.OnDestroyAllBuildings();
+        _buildingsGrid.RemoveGrid();
+        _buildingsGrid.CreateTowerHall();
+        _saveSystem.Save();
+        if (_spawner.ChecForMaximumLevel())
+            _saveSystem.ResetLevel();
+        _spawner.StartLevel();
+        _movingCameraSpawnEnemies.RotationCamera();
     }
 }
