b9a86df6f4f998f62cb3547df075eed314df858e
ValkoHappy <11081108cat@gmail.com>
2023-04-22 15:41:40 +0300

    correction in the ButtleScreen class

--- Assets/Scripts/UI/Screen/ButtleScreen.cs
+++ Assets/Scripts/UI/Screen/ButtleScreen.cs
@@ -7,17 +7,29 @@ public class ButtleScreen : UIScreenAnimator
     [SerializeField] private Button _exitButton;
     [SerializeField] private Button _settingsButton;
 
+    [SerializeField] private EnemyHandler _enemyHandler;
+    [SerializeField] private BuildingsHandler _buildingsHandler;
+    [SerializeField] private Spawner _spawner;
+    [SerializeField] private GroundAudio _groundAudio;
+    [SerializeField] private YandexAds _yandexAds;
+
     public event UnityAction ExitButtonClick;
     public event UnityAction SettingsButtonClick;
 
     private void OnEnable()
     {
+        _enemyHandler.AllEnemiesKilled += CloseScreen;
+        _buildingsHandler.AllBuildingsBroked += CloseScreen;
+
         _exitButton.onClick.AddListener(OnExitButton);
         _settingsButton.onClick.AddListener(OnSettingsButton);
     }
 
     private void OnDisable()
     {
+        _enemyHandler.AllEnemiesKilled -= CloseScreen;
+        _buildingsHandler.AllBuildingsBroked -= CloseScreen;
+
         _exitButton.onClick.RemoveListener(OnExitButton);
         _settingsButton.onClick.RemoveListener(OnSettingsButton);
     }
@@ -25,6 +37,11 @@ public class ButtleScreen : UIScreenAnimator
     public void OnExitButton()
     {
         ExitButtonClick?.Invoke();
+        _enemyHandler.OnDestroyEnemies();
+        _buildingsHandler.OnCreateSavedBuildings();
+        _spawner.StartLevel();
+        _groundAudio.On—almClip();
+        _yandexAds.ShowInterstitial();
     }
 
     public void OnSettingsButton()
